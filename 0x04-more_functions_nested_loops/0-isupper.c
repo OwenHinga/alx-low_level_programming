d4bd49029b82403f2c5e2212ebce76b87974daa4
OwenHinga <hinga139@gmail.com>
2023-07-20 10:20:09 -0700

    more functions and nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
   *_isupper - Checks uppercase letters
-  *
-  *c - char to check
+  *@c: char to check
   *
   *Return: 0 or 1
   */
-int _isupper(int c)
+
+int _isupper(int c);
 
 {
 	if (c >= 'A' && c <= 'Z')
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * mul - multiply 2 integers
- * a: first int
- * b: second int
- * Return: multiplication result
+ * mul - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
  */
 
 int mul(int a, int b)
-
 {
 	return (a * b);
 }
