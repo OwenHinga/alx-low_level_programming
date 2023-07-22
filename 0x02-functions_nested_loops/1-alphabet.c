da3be0705658031d0100c380d88017e05ccaaee2
OwenHinga <hinga139@gmail.com>
2023-07-22 13:22:13 -0700

    print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *main - prints the alphabet in lowercase
+  *Return: Always 0 (Success)
+  */
+int main(void)
+
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
