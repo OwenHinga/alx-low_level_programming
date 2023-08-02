cb76d4c8d43e0ee0240a15e27ebf2cea4dbce298
OwenHinga <hinga139@gmail.com>
2023-08-02 12:20:33 -0700

    recursions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-  *main - prints the alphabet in lowercase
+  * print_alphabet- prints the alphabet in lowercase
   *Return: Always 0 (Success)
   */
-int main(void)
+void print_alphabet(void)
 
 {
-	int i;
+	char i;
 
 	for (i = 97; i <= 122; i++)
 	{
